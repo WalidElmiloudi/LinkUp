1e9ea031744439e3fd34dcd6f2e34036a322711c
WalidElmiloudi <elwalid38@gmail.com>
2026-02-07 19:56:35 +0100

    Removing unecessary method in class User also modifying the method index in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 {
     public function index(): View
     {
-        $posts = Post::all();
+        $posts = Post::latest()->get();
         return view('pages.home',compact('posts'));
     }
 }
